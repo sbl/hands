9f3688bbcbd9247102f4add0ae081458b856d4bf
sbl <stephen.lumenta@gmail.com>
2014-07-20 16:46:48 +0200

    record rw

--- runtime.go
+++ runtime.go
@@ -3,6 +3,7 @@ package hands
 import (
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"time"
 )
 
@@ -13,11 +14,18 @@ const headerRuntime = "X-Runtime"
 func Runtime(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		next.ServeHTTP(w, r)
+		rec := httptest.NewRecorder()
+		next.ServeHTTP(rec, r)
+
+		for k, v := range rec.Header() {
+			w.Header()[k] = v
+		}
 		if w.Header().Get(headerRuntime) == "" {
 			dur := time.Since(start)
 			w.Header().Set(headerRuntime, fmt.Sprintf("%0.6f", dur.Seconds()))
 		}
+		w.WriteHeader(rec.Code)
+		w.Write(rec.Body.Bytes())
 	}
 	return http.HandlerFunc(fn)
 }
