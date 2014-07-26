43f00024a0bf3b2aa0138d60309272072306dcbf
sbl <stephen.lumenta@gmail.com>
2014-07-26 12:34:46 +0200

    export requestID header

--- request_id.go
+++ request_id.go
@@ -6,18 +6,18 @@ import (
 	"code.google.com/p/go-uuid/uuid"
 )
 
-const headerRequestID = "X-Request-ID"
+const HeaderRequestID = "X-Request-ID"
 
 // RequestID sets up "X-Request-Id" header if it has not been previously been
 // set up.
 func RequestID(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		id := r.Header.Get(headerRequestID)
+		id := r.Header.Get(HeaderRequestID)
 		if id == "" {
 			id = uuid.New()
 		}
-		w.Header().Set(headerRequestID, id)
-		r.Header.Set(headerRequestID, id)
+		w.Header().Set(HeaderRequestID, id)
+		r.Header.Set(HeaderRequestID, id)
 
 		next.ServeHTTP(w, r)
 	}
--- request_id_test.go
+++ request_id_test.go
@@ -14,18 +14,18 @@ func TestRequestIDAssignRandomUUID(t *testing.T) {
 	h := RequestID(c)
 	h.ServeHTTP(rec, req)
 
-	id := req.Header.Get(headerRequestID)
+	id := req.Header.Get(HeaderRequestID)
 	if exp, got := 36, len(id); exp != got {
 		t.Errorf("expected id with %d chars, got %d", exp, got)
 	}
 
-	if got := rec.Header().Get(headerRequestID); id != got {
+	if got := rec.Header().Get(HeaderRequestID); id != got {
 		t.Errorf("expected same request-id on req and resp, got %#v", got)
 	}
 
 	req, _ = http.NewRequest("GET", "", nil)
 	h.ServeHTTP(rec, req)
-	if id == req.Header.Get(headerRequestID) {
+	if id == req.Header.Get(HeaderRequestID) {
 		t.Errorf("expected alternating ids, again got: %#v", id)
 	}
 }
@@ -34,18 +34,18 @@ func TestRequestIDPassThruWhenPresent(t *testing.T) {
 	id := "my-personal-very-random-request-id"
 
 	req, _ := http.NewRequest("GET", "", nil)
-	req.Header.Set(headerRequestID, id)
+	req.Header.Set(HeaderRequestID, id)
 	rec := httptest.NewRecorder()
 
 	c := new(counter)
 	h := RequestID(c)
 	h.ServeHTTP(rec, req)
 
-	if got := rec.Header().Get(headerRequestID); id != got {
+	if got := rec.Header().Get(HeaderRequestID); id != got {
 		t.Errorf("expected response request-id %#v, got %#v", id, got)
 	}
 
-	if got := req.Header.Get(headerRequestID); id != got {
+	if got := req.Header.Get(HeaderRequestID); id != got {
 		t.Errorf("expected request request-id %#v, got %#v", id, got)
 	}
 }
